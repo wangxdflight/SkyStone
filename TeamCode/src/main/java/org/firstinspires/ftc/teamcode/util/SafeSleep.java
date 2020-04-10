5a71a458e07b79865046d17b373f6f6455ec0f04
wangx <wxiaodong@gmail.com>
2020-04-09 18:54:12 -0700

    make sleep safe by chehcking opmode regularly

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/SafeSleep.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/SafeSleep.java
@@ -5,12 +5,21 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 public class SafeSleep
 {
     public static void sleep_milliseconds(LinearOpMode mode, int c) {
-        if (mode.opModeIsActive()) {
-            try {
-                Thread.sleep(c);
-            } catch (Exception e) {
-                e.printStackTrace();
+        int r = c, t = 0;
+        while (r > 0) {
+            if (r > 200)
+                t = 200;
+            else
+                t = r;
+            if (mode.opModeIsActive()) {
+                try {
+                    Thread.sleep(t);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
+            r = r - t;
         }
+
     }
 }
